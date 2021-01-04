c43e1fea9e2b0cc6d631c40fd9c88b887bd5a2a0
Hugo Le Morvan <lm.hugo@hotmail.fr>
2021-01-04 10:33:29 +0100

    creation schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_28_203142) do
+ActiveRecord::Schema.define(version: 2021_01_04_092443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "place_id", null: false
+    t.text "description"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["place_id"], name: "index_comments_on_place_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "districts", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string "name"
+    t.integer "price_level"
+    t.bigint "category_id", null: false
+    t.bigint "district_id", null: false
+    t.string "adress"
+    t.float "latitude"
+    t.float "longitude"
+    t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_places_on_category_id"
+    t.index ["district_id"], name: "index_places_on_district_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +65,8 @@ ActiveRecord::Schema.define(version: 2020_12_28_203142) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "places"
+  add_foreign_key "comments", "users"
+  add_foreign_key "places", "categories"
+  add_foreign_key "places", "districts"
 end
