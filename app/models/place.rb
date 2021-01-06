6469628dbd9d843fb7aaeebbcbe7077cf52850fc
Hugo Le Morvan <lm.hugo@hotmail.fr>
2021-01-06 12:13:39 +0100

    ajout geocoder

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,4 +3,6 @@ class Place < ApplicationRecord
   belongs_to :district
   has_many :comments, dependent: :destroy
   validates :name, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
